1402b96e362ddb90d7ae8b0b43f1f559eb53c011
Gustavo <gustavo.h.stahl@unesp.br>
2022-03-06 23:21:31 -0300

    Added the initial implementation of an enemy

---
+++ Assets/Scenes/Scene_0/Scripts/EnemySpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+    public int enemyCount;
+    public float radiusOfSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(enemyDrop());
+    }
+
+    Vector3 RandomNavmeshLocation(float radius) {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) 
+        {
+            finalPosition = hit.position;            
+            finalPosition.y = 0f;
+        }
+        return finalPosition;
+    }
+
+    // Update is called once per frame
+    IEnumerator enemyDrop()
+    {
+        int counter = 0;
+        while(counter < enemyCount)
+        {   
+            Instantiate(enemy, RandomNavmeshLocation(radiusOfSpawn), Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+            counter += 1;
+        }
+    }
+}
